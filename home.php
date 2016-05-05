4536937566e9363e6f8c67abcb5930e1413c2aa8
Hedgeriot <john.mace@ucc.on.ca>
2016-05-05 15:09:01 -0400

    Added home.php
    More to follow!

---
+++ home.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+		<link href="favicon.ico" rel="icon" />
+		<link href="css/bootstrap.min.css" rel="stylesheet" />
+    	<link href="css/font-awesome.min.css" rel="stylesheet" />
+		<link href="css/ourstyle.css" rel="stylesheet" />
+		<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    	<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="UCC-SMD" content="Content???">
+		<title>Norfleet</title>
+</head>
+<body>
+
+	
+
+</body>
+</html>
+
