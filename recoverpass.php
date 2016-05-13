58de48f3a04a1ce3e4f191576a3b885d924d4241
George-Gauvierre6 <charlie.hughes@ucc.on.ca>
2016-05-13 11:50:59 -0400

    Added github, css, and font links

--- recoverpass.php
+++ recoverpass.php
@@ -1,8 +1,12 @@
+<link href="images/web_assets/favicon.ico" rel="icon" />
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/font-awesome.min.css" rel="stylesheet" />
+
 <!-- this will allow the user to recover their password. general outline: two fields in a form, one for username other for email, 
 submit button. use php mail function to email new password to user, and reset it using sql
  -->
 <center><h1>Password Reset</h1>
-<h2>Please enter the email and username of your account; once entered, a new generated-password will be sent to your email.</h2>
+<h2>Please enter the email and username of your account.  Once entered, a new generated-password will be sent to your email.</h2>
 <form action="recoverpass.php" method="post"> 
 Username:<br /> 
 <input type="text" name="username" value="" /> 
