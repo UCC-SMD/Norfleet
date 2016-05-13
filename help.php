98d7a2c36d0f283bc5cf2c323e6cb707ea883eba
George-Gauvierre6 <charlie.hughes@ucc.on.ca>
2016-05-13 12:25:55 -0400

    updated help links

--- help.php
+++ help.php
@@ -2,7 +2,7 @@
 <link href="css/bootstrap.min.css" rel="stylesheet" />
 <link href="css/font-awesome.min.css" rel="stylesheet" />
 
-<center> <h1>Help</h1>
+<center><h1>Help</h1>
     
     <div class="row">
         <div class="col-sm-6 col-md-4">
@@ -11,7 +11,7 @@
                 <div class="CaptionOne">
                     <h3>How to Tweet</h3>
                     <p>Tweeting is the primary method of communication on this website, learn how to 'tweet' here...</p>
-                    <p><a href="helpPages/tweetHelp" class="btn btn-primary" role="button">Tweet Help</a></p>
+                    <p><a href="tweetHelp.php" class="btn btn-primary" role="button">Tweet Help</a></p>
                 </div>
             </div>
         </div>
@@ -21,7 +21,7 @@
                 <div class="CaptionTwo">
                     <h3>Hashtags</h3>
                     <p>Hashtags are keywords used to identify specific points of note, learn how to use them here...</p>
-                    <p><a href="helpPages/hashtagHelp" class="btn btn-primary" role="button">Hashtag Help</a></p>
+                    <p><a href="hashtagHelp.php" class="btn btn-primary" role="button">Hashtag Help</a></p>
                 </div>
             </div>
         </div>
@@ -31,7 +31,7 @@
                 <div class="CaptionThree">
                     <h3>Your Profile</h3>
                     <p>Your profile is who you are, learn how to maximize your personal customization here...</p>
-                    <p><a href="helpPages/profileHelp" class="btn btn-primary" role="button">Profile Help</a></p>
+                    <p><a href="profileHelp.php" class="btn btn-primary" role="button">Profile Help</a></p>
                 </div>
             </div>
         </div>
