0839b304da5a9aeaf3a8b1094d2b688322a36595
fraggrenadekitten <camran.hansen@ucc.on.ca>
2016-05-12 21:46:20 -0400

    Updated registration file with error messages.
    Will talk in class on Friday to discuss exactly how to present error
    messages (as in what style we should use)

--- register.php
+++ register.php
@@ -1,26 +1,76 @@
+<!-- Import da bootstrap -->
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<h1>Register</h1> 
+<form action="register.php" method="post"> 
+    Username:<br /> 
+    <input type="text" name="username" value="" /> 
+    <br /><br /> 
+    E-Mail:<br /> 
+    <input type="text" name="email" value="" /> 
+    <br /><br /> 
+    Password:<br /> 
+    <input type="password" name="password" value="" /> 
+    <br /><br /> 
+    <input type="submit" value="Register" /> 
+    
+</form>
+<div id="alertbox" class="alert alert-danger" style="visibility: hidden"role="alert">
+    <p id="errormessagebox_thing"></p>
+</div>
+<script type="text/javascript">
+    window.errormessages = [];
+    function displayerrormessages() {
+        console.log(errormessages);
+        // for (var i = 0; i<errormessages.length; i++) {
+        //     console.log((errormessages[i]));
+        // };
+        document.getElementById('errormessagebox_thing').innerHTML = "error, must input: " + errormessages;
+        document.getElementById('alertbox').style.visibility = 'visible';
+    }
+</script>
+<!-- Dongers -->
 <?php 
 
     // First we execute our common code to connection to the database and start the session 
     require("common.php"); 
-     
     // This if statement checks to determine whether the registration form has been submitted 
     // If it has, then the registration code is run, otherwise the form is displayed 
+    //$_POST is an associative array 
+    //  ($_POST as $variable) {
+    //     echo $variable;
+    // }
+
     if(!empty($_POST)) 
     { 
+     
         // Ensure that the user has entered a non-empty username 
         if(empty($_POST['username'])) 
         { 
-            // Note that die() is generally a terrible way of handling user errors 
-            // like this.  It is much better to display the error with the form 
-            // and allow the user to correct their mistake.  However, that is an 
-            // exercise for you to implement yourself. 
-            die("Please enter a username."); 
-        } 
-         
+            //javascript code goes here! 
+            echo "<script>
+            errormessages.push('username');
+            displayerrormessages();
+            // alert('test 0');
+            </script>";
+            // echo "errormessagebox_thing";
+
+            // // Note that die() is generally a terrible way of handling user errors 
+            // // like this.  It is much better to display the error with the form 
+            // // and allow the user to correct their mistake.  However, that is an 
+            // // exercise for you to implement yourself. 
+            // die("Please enter a username."); 
+        }
+        
         // Ensure that the user has entered a non-empty password 
         if(empty($_POST['password'])) 
         { 
-            die("Please enter a password."); 
+            echo "<script>
+            errormessages+=' password';
+            // alert('test 1');
+            displayerrormessages();
+            
+            </script>";
         } 
          
         // Make sure the user entered a valid E-Mail address 
@@ -29,7 +79,14 @@
         // http://us.php.net/manual/en/filter.filters.php 
         if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) 
         { 
-            die("Invalid E-Mail Address"); 
+            // echo "<script>alert(\"fuck you\");</script>";
+            echo "<script>
+            errormessages+=' email';  
+            displayerrormessages();
+            // alert('test 3');
+            
+            
+            </script>";
         } 
          
         // We will use this SQL query to see whether the username entered by the 
@@ -65,7 +122,8 @@
         { 
             // Note: On a production website, you should not output $ex->getMessage(). 
             // It may provide an attacker with helpful information about your code.  
-            die("Failed to run query: " . $ex->getMessage()); 
+            //die("Failed to run query: " . $ex->getMessage()); 
+            // echo "<script>alert(\"fuck you\");</script>";
         } 
          
         // The fetch() method returns an array representing the "next" row from 
@@ -76,7 +134,8 @@
         // the database already and we should not allow the user to continue. 
         if($row) 
         { 
-            die("This username is already in use"); 
+            // die("This username is already in use"); 
+            // echo "<script>alert(\"fuck you\");</script>";
         } 
          
         // Now we perform the same type of check for the email address, in order 
@@ -107,7 +166,8 @@
          
         if($row) 
         { 
-            die("This email address is already registered"); 
+            // die("This email address is already registered"); 
+            // echo "<script>alert(\"fuck you\");</script>";
         } 
          
         // An INSERT query is used to add new rows to a database table. 
@@ -174,34 +234,18 @@
         { 
             // Note: On a production website, you should not output $ex->getMessage(). 
             // It may provide an attacker with helpful information about your code.  
-            die("Failed to run query: " . $ex->getMessage()); 
+            // die("Failed to run query: " . $ex->getMessage()); 
+            // echo "<script>alert(\"fuck you\");</script>";
         } 
          
         // This redirects the user back to the login page after they register 
-        header("Location: login.php"); 
+        // header("Location: login.php"); 
          
         // Calling die or exit after performing a redirect using the header function 
         // is critical.  The rest of your PHP script will continue to execute and 
         // will be sent to the user if you do not die or exit. 
-        die("Redirecting to login.php"); 
+        // die("Redirecting to login.php"); 
+        // echo "<script>alert(\"fuck you\");</script>";
     } 
      
 ?> 
-
-<link href="images/web_assets/favicon.ico" rel="icon" />
-<link href="css/bootstrap.min.css" rel="stylesheet" />
-<link href="css/font-awesome.min.css" rel="stylesheet" />
-
-<h1>Register</h1> 
-<form action="register.php" method="post"> 
-    Username:<br /> 
-    <input type="text" name="username" value="" /> 
-    <br /><br /> 
-    E-Mail:<br /> 
-    <input type="text" name="email" value="" /> 
-    <br /><br /> 
-    Password:<br /> 
-    <input type="password" name="password" value="" /> 
-    <br /><br /> 
-    <input type="submit" value="Register" /> 
-</form>
