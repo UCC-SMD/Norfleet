aa006b379ed5d789b38e37d4f723f33d0a77c8b2
George-Gauvierre6 <charlie.hughes@ucc.on.ca>
2016-05-13 10:53:47 -0400

    Update register.php

--- register.php
+++ register.php
@@ -1,4 +1,4 @@
-<!-- Import da bootstrap -->
+<!-- Import the bootstrap -->
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 <h1>Register</h1> 
