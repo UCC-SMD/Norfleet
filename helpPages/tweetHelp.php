4259154b4533ee5a76621905c98f33756a9ab32c
George-Gauvierre6 <charlie.hughes@ucc.on.ca>
2016-05-13 12:24:43 -0400

    Help Pages stuff

---
+++ helpPages/tweetHelp.php
@@ -0,0 +1,86 @@
+<link href="images/web_assets/favicon.ico" rel="icon" />
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/font-awesome.min.css" rel="stylesheet" />
+
+
+    <div id="sidebar-wrapper">
+             <ul class="sidebar-nav">
+                 <li class="sidebar-brand">
+                     <a class="navbar-brand" href="index.php">  <font size="10" color="white"> Norfleet </font>  </a>
+                 </li>
+                 <li>
+                     <a href="javascript:;" data-toggle="collapse" data-target="#demo">
+                         <i class="fa fa-fw fa-user"></i>  Username <i class="fa fa-fw fa-caret-down"></i>
+                       <br>
+                     </a>
+                     <ul id="demo" class="collapse">
+                         <div class="row">
+                             <div class="col-sm-6 col-md-4">
+                                 <div class="thumbnailOne">
+                                    <img src="..." alt="...">
+                                    <div class="CaptionOne">
+                                        <h3>How to Tweet</h3>
+                                        <p>Tweeting is the primary method of communication on this website, learn how to 'tweet' here...</p>
+                                        <p><a href="helpPages/tweetHelp.php" class="btn btn-primary" role="button">Tweet Help</a></p>
+                                     </div>
+                                </div>
+                                 
+                               
+                                 <div class="pageshift">
+                                   <right>
+                                     <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Sidebar</a>
+                                   </right>
+                                 </div>
+                                 
+                                 
+                            </div>
+                            <div class="col-sm-6 col-md-4">
+                                <div class="thumbnailTwo">
+                                <img src="..." alt="...">
+                                    <div class="CaptionTwo">
+                                        <h3>Hashtags</h3>
+                                        <p>Hashtags are keywords used to identify specific points of note, learn how to use them here...</p>
+                                        <p><a href="helpPages/hashtagHelp.php" class="btn btn-primary" role="button">Hashtag Help</a></p>
+                                    </div>
+                                </div>
+                                
+                                
+                                <div class="pageshift">
+                                   <right>
+                                     <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Sidebar</a>
+                                   </right>
+                                 </div>
+                                
+                                
+                            </div>
+                             <div class="col-sm-6 col-md-4">
+                                <div class="thumbnailThree">
+                                    <img src="..." alt="...">
+                                    <div class="CaptionThree">
+                                        <h3>Your Profile</h3>
+                                        <p>Your profile is who you are, learn how to maximize your personal customization here...</p>
+                                        <p><a href="helpPages/profileHelp.php" class="btn btn-primary" role="button">Profile Help</a></p>
+                                    </div>
+                                </div>
+                                 
+                                 
+                                 
+                                 <div class="pageshift">
+                                   <right>
+                                     <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Sidebar</a>
+                                   </right>
+                                 </div>
+                                 
+                                 
+                             </div>
+                         </div>
+                     </ul>
+                 </div>
+
+        <script>
+            //  32 is JavaScript SpaceBar keycode
+            $("#menu-toggle").click(function(e) {
+                e.preventDefault();
+                $("#wrapper").toggleClass("toggled");
+            });
+        </script>
