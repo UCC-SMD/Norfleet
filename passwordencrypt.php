915195833155765a6ec43448af16223123c3a1a9
fraggrenadekitten <camran.hansen@ucc.on.ca>
2016-05-15 14:22:46 -0400

    file systems = †
    the power of christ compels you

--- passwordencrypt.php
+++ passwordencrypt.php
@@ -3,7 +3,6 @@
 function encrypt_password($pas)
 {	
 
-	return "asdf";
 	echo "<script>console.log('doing the encrytption');</script>";
 	$salt = dechex(mt_rand(0, 2147483647)) . dechex(mt_rand(0, 2147483647)); 
 	$password = hash('sha256', $pas . $salt); 
