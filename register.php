ff4fe3b762e7cf21dad51779d94485a51d3c7166
fraggrenadekitten <camran.hansen@ucc.on.ca>
2016-05-15 13:31:35 -0400

    only does query if porridge is medium
    if $cont != FALSE
    
    then do the things

--- register.php
+++ register.php
@@ -24,23 +24,23 @@
     window.errormessages = [];
     function displayerrormessages() {
         console.log(errormessages);
-        var wat = "You forgot to add: ";
+        var total_error_log = "You forgot to add: ";
         for (var i = 0; i<errormessages.length; i++) {
             // /console.log((errormessages[i]));
             if (errormessages.length!=1) {
                 if (i+1 == errormessages.length) {
-                    wat += " and " + errormessages[i] + ".";
+                    total_error_log += " and " + errormessages[i] + ".";
                 } else{
-                    wat += " " + errormessages[i] + ",";
+                    total_error_log += " " + errormessages[i] + ",";
                 };
             }else{
-                wat += errormessages[i];
+                total_error_log += errormessages[i];
             };
             
             
         };
 
-        document.getElementById('errormessagebox_thing').innerHTML = wat;
+        document.getElementById('errormessagebox_thing').innerHTML = total_error_log;
         document.getElementById('alertbox').style.visibility = 'visible';
 
     }
@@ -56,11 +56,10 @@
     //  ($_POST as $variable) {
     //     echo $variable;
     // }
-
+    $cont = TRUE;
     if(!empty($_POST)) 
-    { 
-     
-        // Ensure that the user has entered a non-empty username 
+    {  
+        //zthese 3 if statements are the filters to make sure the person fills out the stuff correctly
         if(empty($_POST['username'])) 
         { 
             //javascript code goes here! 
@@ -70,7 +69,7 @@
             // alert('test 0');
             </script>";
             // echo "errormessagebox_thing";
-
+            $cont = FALSE;
             // // Note that die() is generally a terrible way of handling user errors 
             // // like this.  It is much better to display the error with the form 
             // // and allow the user to correct their mistake.  However, that is an 
@@ -87,12 +86,10 @@
             displayerrormessages();
             
             </script>";
+            $cont = FALSE;
         } 
          
-        // Make sure the user entered a valid E-Mail address 
-        // filter_var is a useful PHP function for validating form input, see: 
-        // http://us.php.net/manual/en/function.filter-var.php 
-        // http://us.php.net/manual/en/filter.filters.php 
+
         if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) 
         { 
             // echo "<script>alert(\"fuck you\");</script>";
@@ -103,12 +100,9 @@
             
             
             </script>";
-        } 
-         
-        // We will use this SQL query to see whether the username entered by the 
-        // user is already in use.  A SELECT query is used to retrieve data from the database. 
-        // :username is a special token, we will substitute a real value in its place when 
-        // we execute the query. 
+            $cont = FALSE;
+        }
+
         $query = " 
             SELECT 
                 1 
@@ -117,13 +111,7 @@
                 username = :username 
         "; 
          
-        // This contains the definitions for any special tokens that we place in 
-        // our SQL query.  In this case, we are defining a value for the token 
-        // :username.  It is possible to insert $_POST['username'] directly into 
-        // your $query string; however doing so is very insecure and opens your 
-        // code up to SQL injection exploits.  Using tokens prevents this. 
-        // For more information on SQL injections, see Wikipedia: 
-        // http://en.wikipedia.org/wiki/SQL_Injection 
+       
         $query_params = array( 
             ':username' => $_POST['username'] 
         ); 
@@ -140,6 +128,7 @@
             // It may provide an attacker with helpful information about your code.  
             //die("Failed to run query: " . $ex->getMessage()); 
             // echo "<script>alert(\"fuck you\");</script>";
+            $cont = FALSE;
         } 
          
         // The fetch() method returns an array representing the "next" row from 
@@ -150,7 +139,8 @@
         // the database already and we should not allow the user to continue. 
         if($row) 
         { 
-            // die("This username is already in use"); 
+            // die("This username is already in use");
+            $cont = FALSE;
             // echo "<script>alert(\"fuck you\");</script>";
         } 
          
@@ -165,7 +155,7 @@
         "; 
          
         $query_params = array( 
-            ':email' => $_POST['email'] 
+            ':email' => $_POST['email']
         ); 
          
         try 
@@ -184,6 +174,7 @@
         { 
             // die("This email address is already registered"); 
             // echo "<script>alert(\"fuck you\");</script>";
+            $cont = FALSE;
         } 
          
         // An INSERT query is used to add new rows to a database table. 
@@ -212,19 +203,20 @@
             ':email' => $_POST['email'] 
         ); 
          
-        try 
-        { 
-            // Execute the query to create the user 
-            $stmt = $db->prepare($query); 
-            $result = $stmt->execute($query_params); 
-        } 
-        catch(PDOException $ex) 
-        { 
-            // Note: On a production website, you should not output $ex->getMessage(). 
-            // It may provide an attacker with helpful information about your code.  
-            // die("Failed to run query: " . $ex->getMessage()); 
-            // echo "<script>alert(\"fuck you\");</script>";
-        } 
+        if($cont==TRUE){
+            try{ 
+                // Execute the query to create the user 
+                $stmt = $db->prepare($query); 
+                $result = $stmt->execute($query_params); 
+            } 
+            catch(PDOException $ex) 
+            { 
+                // Note: On a production website, you should not output $ex->getMessage(). 
+                // It may provide an attacker with helpful information about your code.  
+                die("Failed to run query: " . $ex->getMessage()); 
+                // echo "<script>alert(\"fuck you\");</script>";
+            } 
+        }
          
         // This redirects the user back to the login page after they register 
         // header("Location: login.php"); 
@@ -234,6 +226,6 @@
         // will be sent to the user if you do not die or exit. 
         // die("Redirecting to login.php"); 
         // echo "<script>alert(\"fuck you\");</script>";
-    } 
+    }//if !empty post
      
 ?> 
