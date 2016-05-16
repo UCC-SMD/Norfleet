3bac4822b743752cceedac5d1635b4484416dfb6
fraggrenadekitten <camran.hansen@ucc.on.ca>
2016-05-16 19:04:39 -0400

    made this file better
    by making no swearing.

--- asdf.php
+++ asdf.php
@@ -1,5 +1,6 @@
 <?php
 include 'passwordencrypt.php';
-$passwordsystem = encrypt_password("fuck_la");
+$passwordsystem = encrypt_password("asdf");
 echo $passwordsystem[0]. "   " . $passwordsystem[1];
+//this is just a testing file, no real importance
 ?>
