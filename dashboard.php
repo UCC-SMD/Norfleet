9e17cea6d6a4f40967bc9608d2186a60a5040fef
fraggrenadekitten <camran.hansen@ucc.on.ca>
2016-05-17 19:05:47 -0400

    useless code.

--- dashboard.php
+++ dashboard.php
@@ -300,28 +300,3 @@ $arr = array_values($_SESSION['user']); echo "Welcome " . $arr[1]; ?> </font><sm
  </body>
 
  </html>
-
-<?php
-
-require("common.php"); 
-        
-        // if(empty($_SESSION['user'])) { 
-  
-        //     // If they are not, we redirect them to the login page. 
-        //     $location = "http://" . $_SERVER['HTTP_HOST'] . "/norfleet/dashboard.php";
-        //     echo '<META HTTP-EQUIV="refresh" CONTENT="0;URL='.$location.'">';
-        //     //exit;
-         
-        //     // Remember that this die statement is absolutely critical.  Without it, 
-        //     // people can view your members-only content without logging in. 
-        //     die("Redirecting to login.php"); 
-        // } 
-        
-        // // To access $_SESSION['user'] values put in an array, show user his username
-        $arr = array_values($_SESSION['user']);
-        // echo "Welcome " . $arr[1];
-        // Matt's version
-        // echo "$_SESSION['user']['username'];"
-
-
-?> 
