89077a09257d09851ec4be06fdf34874fa17a9aa
fraggrenadekitten <camran.hansen@ucc.on.ca>
2016-05-17 18:52:11 -0400

    idk whats happening

--- recoverpass.php
+++ recoverpass.php
@@ -1,10 +1,6 @@
-<link href="images/web_assets/favicon.ico" rel="icon" />
+<!-- <link href="images/web_assets/favicon.ico" rel="icon" />
 <link href="css/bootstrap.min.css" rel="stylesheet" />
 <link href="css/font-awesome.min.css" rel="stylesheet" />
-
-<!-- this will allow the user to recover their password. general outline: two fields in a form, one for username other for email, 
-submit button. use php mail function to email new password to user, and reset it using sql
- -->
 <center><h1>Password Reset</h1>
 <h2>Please enter the email and username of your account.  Once entered, a new generated-password will be sent to your email.</h2>
 <form action="recoverpass.php" method="post"> 
@@ -14,14 +10,14 @@ Username:<br />
 E-Mail:<br /> 
 <input type="text" name="email" value="" /> 
 <br /><br /> 
-
 <input type="submit" value="recover password" /> 
-
 </form>
 </center>
+ -->
+ <!-- this will allow the user to recover their password. general outline: two fields in a form, one for username other for email, 
+submit button. use php mail function to email new password to user, and reset it using sql
+ -->
 <?php
-// il add more later
-include 'passwordencrypt.php';
-require("common.php"); 
-echo encrypt_password("test");
+mail("camran.hansen@gmail.com", "fuck", "fuck");
 ?> 
+
