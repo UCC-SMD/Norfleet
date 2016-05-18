d2e17d0e155bc7901e4399d7cd8f2e649e174341
fraggrenadekitten <camran.hansen@ucc.on.ca>
2016-05-18 19:54:15 -0400

    Password email started.

--- recoverpass.php
+++ recoverpass.php
@@ -1,4 +1,4 @@
-<!-- <link href="images/web_assets/favicon.ico" rel="icon" />
+<link href="images/web_assets/favicon.ico" rel="icon" />
 <link href="css/bootstrap.min.css" rel="stylesheet" />
 <link href="css/font-awesome.min.css" rel="stylesheet" />
 <center><h1>Password Reset</h1>
@@ -13,43 +13,21 @@ E-Mail:<br />
 <input type="submit" value="recover password" /> 
 </form>
 </center>
- -->
+
  <!-- this will allow the user to recover their password. general outline: two fields in a form, one for username other for email, 
 submit button. use php mail function to email new password to user, and reset it using sql
  -->
 <?php
-require 'PHPMailerAutoload.php';
 
-$mail = new PHPMailer;
+function sendpass()
+{
+	$to = "";
+	$subject = "Password Retrieval";
+	$txt = "";
+	$headers = "From: passwordretrieval.noreply@norfleet.com";
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'user@example.com';                 // SMTP username
-$mail->Password = 'secret';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 8888;                                    // TCP port to connect to
-
-$mail->setFrom('from@example.com', 'Mailer');
-$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-$mail->addAddress('ellen@example.com');               // Name is optional
-$mail->addReplyTo('info@example.com', 'Information');
-$mail->addCC('cc@example.com');
-$mail->addBCC('bcc@example.com');
-
-$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message has been sent';
+	mail($to,$subject,$txt,$headers);
 }
-?> 
 
+sendpass();
+?>
