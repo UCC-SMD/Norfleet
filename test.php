7236bc1476cb58c623cf8a4bac38d77d7a88e968
fraggrenadekitten <camran.hansen@ucc.on.ca>
2016-05-18 14:32:27 -0400

    start with weather

--- test.php
+++ test.php
@@ -1,19 +1,15 @@
 <?php
-
-require("common.php"); 
-        
-        if(empty($_SESSION['user'])) { 
-  
-            // If they are not, we redirect them to the login page. 
-            $location = "http://" . $_SERVER['HTTP_HOST'] . "/norfleet";
-            echo '<META HTTP-EQUIV="refresh" CONTENT="0;URL='.$location.'">';
-            //exit;
-         
-            // Remember that this die statement is absolutely critical.  Without it, 
-            // people can view your members-only content without logging in. 
-            die("Redirecting to login.php"); 
-        } 
-        
-$arr = array_values($_SESSION['user']); echo $arr[1]; 
-?> 
-
+$city="Delhi";
+$country="IN"; //Two digit country code
+$url="http://api.openweathermap.org/data/2.5/weather?q=".$city.",".$country."&units=metric&cnt=7&lang=en";
+$json=file_get_contents($url);
+$data=json_decode($json,true);
+//Get current Temperature in Celsius
+echo $data['main']['temp']."<br>";
+//Get weather condition
+echo $data['weather'][0]['main']."<br>";
+//Get cloud percentage
+echo $data['clouds']['all']."<br>";
+//Get wind speed
+echo $data['wind']['speed']."<br>";
+?>
