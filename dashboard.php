88a53a62e31a8fa94b8a195bf1c394871150fe0c
fraggrenadekitten <camran.hansen@ucc.on.ca>
2016-05-18 20:16:40 -0400

    put back

--- dashboard.php
+++ dashboard.php
@@ -63,6 +63,17 @@
 
                          <i class="fa fa-fw fa-user">
                         </i> <?php require("common.php");//this code is so the username is displayed.
+                        if(empty($_SESSION['user'])) { 
+
+                            // If they are not, we redirect them to the login page. 
+                            $location = "http://" . $_SERVER['HTTP_HOST'] . "/index.php";
+                            echo '<META HTTP-EQUIV="refresh" CONTENT="0;URL='.$location.'">';
+                            //exit;
+                         
+                            // Remember that this die statement is absolutely critical.  Without it, 
+                            // people can view your members-only content without logging in. 
+                            die("Redirecting so you can log in"); 
+                        }
 
 
                         // To access $_SESSION['user'] values put in an array, show user his username
