5d558c87e7fd6cefe4007cd177e7c47b56fdd796
Axler34 <campbell.mackinnon@ucc.on.ca>
2016-05-19 00:04:26 -0400

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,12 @@
+<!--
+Programmed by Camran on May 16th, 2016
+For NORFLEET, social media for professionals
+Full team: Charlie, Campbell, Camran and John
+Object of code: Working on using an API to get weather on the wedsite (not sure if this is fully implemented yet)
+*please note* Any code that is commented out is things that we either: #1 not working properly, #2 features we would like to implement at a
+later time or #3 old outdated code we haven't gotten around to cleaning up yet (we're lazy xD)
+-->
+
 <?php
 $city="Delhi";
 $country="IN"; //Two digit country code
