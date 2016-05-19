61d8f5e4f624f4f6e03c6023337534364f204e2a
Axler34 <campbell.mackinnon@ucc.on.ca>
2016-05-18 23:53:02 -0400

    Commented and bannered

--- error.php
+++ error.php
@@ -1,3 +1,12 @@
+<!--
+Programmed by Campbell on May 16th, 2016
+For NORFLEET, social media for professionals
+Full team: Charlie, Campbell, Camran and John
+Object of code: This is an error page that you are redirected to if something goes wrong
+*please note* Any code that is commented out is things that we either: #1 not working properly, #2 features we would like to implement at a
+later time or #3 old outdated code we haven't gotten around to cleaning up yet (we're lazy xD)
+-->
+
 <html>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
@@ -17,12 +26,14 @@ table {
 
 </style>
 
+<!-- adding the favicon -->
 <link rel="icon" href="sound/favicon.ico">
 
 <center>
 
 <title>V I B E S</title>
 
+<!-- setting the background image -->
 	<body background="sound/shartcode_background.jpg">
     
 	<i> <h1>Uh oh! Something went wrong :( Listen to this song by Macintosh Plus to relax and decompress ( ͡° ͜ʖ ͡°)</h1> </i>
@@ -33,6 +44,7 @@ table {
 
 <link rel="icon" href="sound/favicon.ico">
 
+<!-- loads in the audio file for a song -->
 <script type="text/javascript">
 	<script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
     <script>
